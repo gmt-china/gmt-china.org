795b400d5e6b2ff86057eb35a140c25fd2ca9537
Dongdong Tian <seisman.info@gmail.com>
2016-11-24 10:40:35 +0800

    fix

--- content/example/ex010/topo.bat
+++ content/example/ex010/topo.bat
@@ -1,13 +1,13 @@
-gmt gmtset FORMAT_GEO_MAP=ddd:mm:ssF
 set PS="topo.ps"
 set eqfile=eq.dat
 set R=70/135/15/55
 
-gmt grdcut ETOPO1_Ice_g_gmt4.grd -R%R% -GcutTopo.grd
+gmt grdcut ETOPO1_Ice.grd -R%R% -GcutTopo.grd
 gmt grdgradient cutTopo.grd -Ne0.7 -A50 -GcutTopo_i.grd
 gmt grd2cpt cutTopo.grd -Cglobe -S-10000/10000/200 -Z -D>colorTopo.cpt
 
 REM »æÖÆµ×Í¼
+gmt gmtset FORMAT_GEO_MAP=ddd:mm:ssF
 gmt psbasemap -R%R% -JM7i -Bf5a10 -BWesN -Xc -Yc -K > %PS%
 gmt grdimage cutTopo.grd -IcutTopo_i.grd -R -J -CcolorTopo.cpt -Q -O -K >>%PS%
 gmt pscoast -R -J -Dh -W1/0.2p -I1/0.25p -N1/0.5p -O -K >>%PS%
@@ -23,15 +23,15 @@ gawk "{if($3>=8.0) print $1,$2,$3*0.06}" %eqfile% | gmt psxy -R -J -Sa -Gpurple 
 
 REM »æÖÆÍ¼Àý
 echo G 0.01i >legend.txt
-echo H 10 4 MAGNITUDE >>legend.txt
+echo H 8 4 MAGNITUDE >>legend.txt
 echo C 0/0/255 >>legend.txt
-echo S 0.05i c 0.20 blue 0.25p,blue 0.18i 5~5.9 >>legend.txt
+echo S 0.1i c 0.20 blue 0.25p,blue 0.18i 5~5.9 >>legend.txt
 echo C 255/0/0 >>legend.txt
-echo S 0.05i c 0.24 red 0.25p,red 0.18i 6~6.9 >>legend.txt
+echo S 0.1i c 0.24 red 0.25p,red 0.18i 6~6.9 >>legend.txt
 echo C 0/255/0 >>legend.txt
-echo S 0.05i a 0.42 green 0.25p,black 0.18i 7~7.9 >>legend.txt
+echo S 0.1i a 0.42 green 0.25p,black 0.18i 7~7.9 >>legend.txt
 echo C purple >>legend.txt
-echo S 0.05i a 0.48 purple 0.25p,black 0.18i 8~8.9 >>legend.txt
+echo S 0.1i a 0.48 purple 0.25p,black 0.18i 8~8.9 >>legend.txt
 gmt pslegend legend.txt -R -J -DjBR+w0.8i+l1.2+o0 -F+g229+p0.25p -O -K >> %PS%
 
 gmt psxy -R -J -T -O >>%PS%

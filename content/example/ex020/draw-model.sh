2936163d8d570e3cb297d5d763a04b8ad22f819f
zhaozhiyuan <shetu2008@163.com>
2018-04-18 15:12:38 +0800

    update

--- content/example/ex020/draw-model.sh
+++ content/example/ex020/draw-model.sh
@@ -1,6 +1,8 @@
+gmt set MAP_TICK_LENGTH_PRIMARY -0.1c
 model=model.txt
 PS=model.ps
-gmt psbasemap -R0/9/0/55 -JX8c/-12c -Bx1+l"velocity (km/s)" -By10+l"depth (km)" -BWsNe -K -P > $PS
+gmt psbasemap -R0/10/0/50 -JX8c/-10c -Bx1+l"velocity (km/s)" -By10+l"depth (km)" -BWN -K -P > $PS
+gmt psbasemap -R -J -Bse -B0 -K -O >> $PS
 # Vs
 awk 'BEGIN{d1=0;d2=0}{d2=d2+$1;print $2,d1;print $2,d2;d1=d2;}' $model | \
 gmt psxy -R -J -W1.5p,blue -K -O >> $PS
@@ -8,7 +10,7 @@ gmt psxy -R -J -W1.5p,blue -K -O >> $PS
 awk 'BEGIN{d1=0;d2=0}{d2=d2+$1;print $3,d1;print $3,d2;d1=d2;}' $model | \
 gmt psxy -R -J -W1.5p,green,6_2:2 -K -O >> $PS
 gmt pstext -R -J -O >> $PS <<EOF
-1.5 50  Model
+1.5 45  Model
 2.5 10  @;blue;Vs@;;
   5 10  @;green;Vp@;;
 EOF

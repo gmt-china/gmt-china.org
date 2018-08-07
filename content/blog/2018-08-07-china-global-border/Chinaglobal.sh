565a253214a1104b1488f154ae8b34a0c5de8d4e
wangliang <wangliang0222@gmail.com>
2018-08-08 00:13:24 +0800

    同时正确画中国和邻国国界

---
+++ content/blog/2018-08-07-china-global-border/Chinaglobal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+R=45/165/-2/55
+J=M104/29/15c
+PS=CN-global-border.ps
+
+gmt psxy -J$J -R$R -T -K > $PS
+gmt psbasemap -R -J -Bxaf -Byaf -BWSEN -P -K -O >> $PS
+
+# 画出中国邻区国界
+gmt select nb.dat -Fquakes.dat -fg | gmt psxy -J -R -W0.5p,black -K -O >> $PS
+# 画出中国邻区外国界
+gmt select gadm36_0.gmt -Fquakes.dat -fg -If | gmt psxy -J -R -W0.5p,black -K -O >> $PS
+
+gmt psxy -J$J -R$R -T -O >> $PS
+gmt psconvert -A -P -Tg $PS -Z
+rm gmt.history
+rm *.ps

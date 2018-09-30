a6036f3dc7d488cc221861d98121f9c95178e92b
wangliang <wangliang0222@gmail.com>
2018-09-30 13:05:34 +0800

    modernize to 6

--- content/example/ex003/CN-border-JM.sh
+++ content/example/ex003/CN-border-JM.sh
@@ -3,15 +3,15 @@ R=70/138/13/56
 J=M105/35/6.5i
 PS=CN-border-JM.ps
 
+gmt begin CN-border-JM png
 gmt set MAP_GRID_PEN_PRIMARY 0.25p,gray,2_2:1
 gmt set FORMAT_GEO_MAP ddd:mm:ssF MAP_FRAME_WIDTH 3p
 gmt set FONT_ANNOT_PRIMARY 7p
 # 设置比例尺标签为35号中文字体
 gmt set FONT_LABEL 8p,35 MAP_LABEL_OFFSET 4p
-
 # 绘制中国地图
-gmt pscoast -J$J -R$R -G244/243/239 -S167/194/223 -B10f5g10 -Lg85/17.5+c17.5+w800k+f+u+l'比例尺' -K > $PS
-gmt psxy CN-border-La.dat -J$J -R$R -W0.5p -O -K >> $PS
+gmt coast -JM105/35/6.5i -R70/138/13/56 -G244/243/239 -S167/194/223 -B10f5g10 -Lg85/17.5+c17.5+w800k+f+u+l'比例尺'
+gmt plot CN-border-La.dat -W0.5p 
 
 # 绘制南海区域
 R=105/123/3/24

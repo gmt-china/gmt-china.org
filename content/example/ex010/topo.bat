9e6bbe5c203034a6aeecb8d1975fd7d3bb53ffa2
wangliang <wangliang0222@gmail.com>
2018-10-01 21:35:55 +0800

    fix

--- content/example/ex010/topo.bat
+++ content/example/ex010/topo.bat
@@ -1,37 +1,36 @@
-set PS="topo.ps"
 set eqfile=eq.dat
-set R=70/135/15/55
 set topodata="earth_relief_06m.grd"
 
-gmt grdcut %topodata% -R%R% -GcutTopo.grd
+gmt grdcut %topodata% -R70/135/15/55 -GcutTopo.grd
 gmt grdgradient cutTopo.grd -Ne0.7 -A50 -GcutTopo_i.grd
 gmt grd2cpt cutTopo.grd -Cglobe -S-10000/10000/200 -Z -D>colorTopo.cpt
 
-REM »æÖÆµ×Í¼
+gmt begin topo png
+REM ï¿½ï¿½ï¿½Æµï¿½Í¼
 gmt gmtset FORMAT_GEO_MAP=ddd:mm:ssF
-gmt psbasemap -R%R% -JM7i -Bf5a10 -BWesN -Xc -Yc -K > %PS%
-gmt grdimage cutTopo.grd -IcutTopo_i.grd -R -J -CcolorTopo.cpt -Q -O -K >>%PS%
-gmt pscoast -R -J -Dh -W1/0.2p -I1/0.25p -N1/0.5p -O -K >>%PS%
+gmt basemap -R70/135/15/55 -JM7i -Bf5a10 -BWesN -Xc -Yc
+gmt grdimage cutTopo.grd -IcutTopo_i.grd -CcolorTopo.cpt -Q
+gmt coast -Dh -W1/0.2p -I1/0.25p -N1/0.5p
 
-REM »æÖÆcolorbar
-gmt psscale -DjCB+w7i/0.15i+o0/-0.5i+h -R -J -CcolorTopo.cpt -Bxa2000f400+l"Elevation/m" -G-8000/8000 -O -K>>%PS%
+REM ï¿½ï¿½ï¿½ï¿½colorbar
+gmt colorbar -DjCB+w7i/0.15i+o0/-0.5i+h -CcolorTopo.cpt -Bxa2000f400+l"Elevation/m" -G-8000/8000
 
-REM ·ÖÕð¼¶»æÖÆµØÕð
+REM ï¿½ï¿½ï¿½ð¼¶»ï¿½ï¿½Æµï¿½ï¿½ï¿½
 gawk "{if(($3>=5.0)&&($3<6.0)) print $1,$2,$3*0.04}" %eqfile% > tmp
-REM Í³¼Æ5¼¶µØÕð¸öÊý
+REM Í³ï¿½ï¿½5ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 for /f "delims=:" %%a in ('findstr/n .* "tmp"') do set M5=%%a
-gmt psxy -R -J tmp -Sc -Gblue -O -K  >> %PS%
+gmt plot tmp -Sc -Gblue
 gawk "{if(($3>=6.0)&&($3<7.0)) print $1,$2,$3*0.04}" %eqfile% > tmp
 for /f "delims=:" %%a in ('findstr/n .* "tmp"') do set M6=%%a
-gmt psxy -R -J tmp -Sc -Gred -O -K >> %PS%
+gmt plot tmp -Sc -Gred
 gawk "{if(($3>=7.0)&&($3<8.0)) print $1,$2,$3*0.06}" %eqfile% > tmp
 for /f "delims=:" %%a in ('findstr/n .* "tmp"') do set M7=%%a
-gmt psxy -R -J tmp -Sa -Ggreen -W0.4p,black -O -K >> %PS%
+gmt plot tmp -Sa -Ggreen -W0.4p,black
 gawk "{if($3>=8.0) print $1,$2,$3*0.06}" %eqfile% > tmp
 for /f "delims=:" %%a in ('findstr/n .* "tmp"') do set M8=%%a
-gmt psxy -R -J tmp -Sa -Gpurple -W0.4p,black -O -K >> %PS%
+gmt plot tmp -Sa -Gpurple -W0.4p,black
 
-REM »æÖÆÍ¼Àý
+REM ï¿½ï¿½ï¿½ï¿½Í¼ï¿½ï¿½
 echo G 0.01i >legend.txt
 echo H 8 4 MAGNITUDE >>legend.txt
 echo C 0/0/255 >>legend.txt
@@ -42,9 +41,7 @@ echo C 0/255/0 >>legend.txt
 echo S 0.1i a 0.42 green 0.25p,black 0.18i 7~7.9(%M7%) >>legend.txt
 echo C purple >>legend.txt
 echo S 0.1i a 0.48 purple 0.25p,black 0.18i 8~8.9(%M8%) >>legend.txt
-gmt pslegend legend.txt -R -J -DjBR+w1.2i+l1.2+o0 -F+g229+p0.25p -O -K >> %PS%
+gmt legend legend.txt -DjBR+w1.2i+l1.2+o0 -F+g229+p0.25p
 
-gmt psxy -R -J -T -O >>%PS%
-gmt psconvert %PS% -A -Tg -P -Z
-
-del .gmt* gmt.* cutTopo*.grd colorTopo.cpt tmp legend.txt
+del cutTopo*.grd colorTopo.cpt tmp legend.txt
+gmt end

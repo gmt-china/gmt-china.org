eb5f2e6291a08393086f2665a5c42d74336382a3
王亮 <wangliang0222@gmail.com>
2018-10-06 02:38:07 +0800

    Modernize ex18 (#97)

--- content/example/ex016/BLOCK.bat
+++ content/example/ex016/BLOCK.bat
@@ -1,48 +1,42 @@
+gmt begin BLOCK png
 gmt gmtset MAP_ANNOT_OBLIQUE 6
 gmt gmtset MAP_TICK_LENGTH_PRIMARY 0
 gmt gmtset MAP_FRAME_TYPE plain
 
-set R=78/12/149/53r
-set J=B105/10/25/47/15c
-set PS=BLOCK.ps
+REM ============ï¿½ï¿½Í¼
+gmt basemap -R78/12/149/53r -JB105/10/25/47/15c -Bx10 -By5 -BNEWS
+gmt coast -Ggray95 -S83/216/238 -A5000 -Dh
+gmt plot CN-border-L1.dat -W0.2p -Gwhite
 
-gmt psxy -R%R% -J%J% -T -K > %PS%
+REM ============ï¿½ï¿½ï¿½Æ°ï¿½ï¿½ß½ï¿½
+gmt plot CN-plate-neighbor.dat -W2.0p,2/138/210 -Sf0.5+t+l -G2/138/210
+REM ============ï¿½ï¿½ï¿½ï¿½ï¿½Æ¶ÏµØ¿ï¿½ß½ï¿½
+gmt plot CN-block-L1-deduced.dat -W1.0p,2/138/210,-
+REM ============ï¿½ï¿½ï¿½Æ¶ï¿½ï¿½ï¿½ï¿½Ø¿ï¿½ß½ï¿½
+gmt plot CN-block-L2.dat -W1.0p,orange
+REM ============ï¿½ï¿½ï¿½ï¿½Ò»ï¿½ï¿½ï¿½Ø¿ï¿½ß½ï¿½
+gmt plot CN-block-L1.dat -W1.0p,2/138/210
 
-REM ============µ×Í¼
-gmt psbasemap -R%R% -J%J% -Bx10 -By5 -BNEWS -K -O >> %PS%
-gmt pscoast -R%R% -J%J% -Ggray95 -S83/216/238 -A5000 -Dh -K -O>> %PS%
-gmt psxy CN-border-L1.dat -R%R% -J%J% -W0.2p -Gwhite -O -K >> %PS%
+REM ============ï¿½î¶¯ï¿½Ø¿ï¿½ï¿½×¢
+echo 90 45 ï¿½ï¿½ï¿½ï¿½î¶¯ï¿½Ø¿ï¿½ > blockname
+echo 122 46 ï¿½ï¿½ï¿½ï¿½ï¿½Ç»î¶¯ï¿½Ø¿ï¿½ >> blockname
+echo 120 35 ï¿½ï¿½ï¿½ï¿½ï¿½î¶¯ï¿½Ø¿ï¿½ >> blockname
+echo 110 27 ï¿½ï¿½ï¿½Ï»î¶¯ï¿½Ø¿ï¿½ >> blockname
+echo 90 33 ï¿½ï¿½Ø»î¶¯ï¿½Ø¿ï¿½ >> blockname
+echo 97 23 ï¿½ï¿½ï¿½ï¿½î¶¯ï¿½Ø¿ï¿½ >> blockname
+gmt text blockname -F+f10p,37
 
-REM ============»æÖÆ°å¿é±ß½ç
-gmt psxy CN-plate-neighbor.dat -R%R% -J%J% -W2.0p,2/138/210 -Sf0.5+t+l -G2/138/210 -O -K >> %PS%
-REM ============»æÖÆÍÆ¶ÏµØ¿é±ß½ç
-gmt psxy CN-block-L1-deduced.dat -R%R% -J%J% -W1.0p,2/138/210,- -O -K >> %PS%
-REM ============»æÖÆ¶þ¼¶µØ¿é±ß½ç
-gmt psxy CN-block-L2.dat -R%R% -J%J% -W1.0p,orange -O -K >> %PS%
-REM ============»æÖÆÒ»¼¶µØ¿é±ß½ç
-gmt psxy CN-block-L1.dat -R%R% -J%J% -W1.0p,2/138/210 -O -K >> %PS%
+REM ============ï¿½ß½ï¿½Í¼ï¿½ï¿½
+echo S 0.3i f0.5+t+l 0.4i/0.3c 2/138/210 2.0p,2/138/210 0.7i ï¿½ï¿½ï¿½ß½ï¿½ > legend
+echo S 0.3i - 0.50i 2/138/210 1.0p,2/138/210 0.7i  Ò»ï¿½ï¿½ï¿½Ø¿ï¿½ß½ï¿½ >> legend
+echo S 0.3i - 0.44i - 1.0p,2/138/210,- 0.7i  ï¿½Æ¶ÏµØ¿ï¿½ß½ï¿½ >> legend
+echo S 0.3i - 0.50i orange 1.0p,orange 0.7i ï¿½ï¿½ï¿½ï¿½ï¿½Ø¿ï¿½ß½ï¿½ >> legend
+gmt legend legend -DjTL+w1.5i+jTL+o0.1c/0.1c -F+gwhite+p0.5p -O -K --FONT_ANNOT_PRIMARY=8p,37 >> %PS%
 
-REM ============»î¶¯µØ¿é±ê×¢
-echo 90 45 Î÷Óò»î¶¯µØ¿é > blockname
-echo 122 46 ¶«±±ÑÇ»î¶¯µØ¿é >> blockname
-echo 120 35 »ª±±»î¶¯µØ¿é >> blockname
-echo 110 27 »ªÄÏ»î¶¯µØ¿é >> blockname
-echo 90 33 Çà²Ø»î¶¯µØ¿é >> blockname
-echo 97 23 µáÃå»î¶¯µØ¿é >> blockname
-gmt pstext blockname -R%R% -J%J% -F+f10p,37 -O -K >> %PS%
-
-REM ============±ß½çÍ¼Àý
-echo S 0.3i f0.5+t+l 0.4i/0.3c 2/138/210 2.0p,2/138/210 0.7i °å¿é±ß½ç > legend
-echo S 0.3i - 0.50i 2/138/210 1.0p,2/138/210 0.7i  Ò»¼¶µØ¿é±ß½ç >> legend
-echo S 0.3i - 0.44i - 1.0p,2/138/210,- 0.7i  ÍÆ¶ÏµØ¿é±ß½ç >> legend
-echo S 0.3i - 0.50i orange 1.0p,orange 0.7i ¶þ¼¶µØ¿é±ß½ç >> legend
-gmt pslegend legend -R%R% -J%J% -DjTL+w1.5i+jTL+o0.1c/0.1c -F+gwhite+p0.5p -O -K --FONT_ANNOT_PRIMARY=8p,37 >> %PS%
-
-REM ============ÄÏº£Öîµº
-gmt psbasemap -R%R% -J%J% -DjBR+w2c+t -K -O >> %PS%
-gmt pscoast -R106/121/3/24 -JM2c -Ggray95 -S83/216/238  -A5000 -Dh -K -O >> %PS%
-gmt psxy CN-border-L1.dat -R106/121/3/24 -JM2c -B0 -W0.2p -Gwhite -O -K >> %PS%
-
-gmt psxy -R%R% -J%J% -T -O >> %PS%
-del gmt.* blockname legend
+REM ============ï¿½Ïºï¿½ï¿½îµº
+gmt basemap -DjBR+w2c+t
+gmt coast -R106/121/3/24 -JM2c -Ggray95 -S83/216/238  -A5000 -Dh
+gmt plot CN-border-L1.dat -R106/121/3/24 -JM2c -B0 -W0.2p -Gwhite
+del blockname legend
+gmt end
 pause
--- content/example/ex016/BLOCK.sh
+++ content/example/ex016/BLOCK.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
+
+gmt begin BLOCK png
 gmt gmtset MAP_ANNOT_OBLIQUE 6
 gmt gmtset MAP_TICK_LENGTH_PRIMARY 0
 gmt gmtset MAP_FRAME_TYPE plain
 
-R=78/12/149/53r
-J=B105/10/25/47/15c
-PS=BLOCK.ps
-
-gmt psxy -R$R -J$J -T -K > $PS
-
 # ============底图
-gmt psbasemap -R$R -J$J -Bx10 -By5 -BNEWS -K -O >> $PS
-gmt pscoast  -R$R -J$J -Ggray95 -S83/216/238 -A5000 -Dh -K -O>> $PS
-gmt psxy CN-border-L1.dat -R$R -J$J -W0.2p -Gwhite -O -K >> $PS
+gmt basemap -R78/12/149/53+r -JB105/10/25/47/15 -Bx10 -By5 -BNEWS
+gmt coast -Ggray95 -S83/216/238 -A5000 -Dh
+gmt plot CN-border-L1.dat -W0.2p -Gwhite
 
 # ============绘制板块边界
-gmt psxy CN-plate-neighbor.dat  -R$R -J$J -W2.0p,2/138/210 -Sf0.5+t+l -G2/138/210 -O -K >> $PS
+gmt plot CN-plate-neighbor.dat -W2.0p,2/138/210 -Sf0.5+t+l -G2/138/210
 # ============绘制推断地块边界
-gmt psxy CN-block-L1-deduced.dat -R$R -J$J -W1.0p,2/138/210,- -O -K >> $PS
+gmt plot CN-block-L1-deduced.dat -W1.0p,2/138/210,-
 # ============绘制二级地块边界
-gmt psxy CN-block-L2.dat -R$R -J$J -W1.0p,orange -O -K >> $PS
+gmt plot CN-block-L2.dat -W1.0p,orange
 # ============绘制一级地块边界
-gmt psxy CN-block-L1.dat -R$R -J$J -W1.0p,2/138/210 -O -K >> $PS
+gmt plot CN-block-L1.dat -W1.0p,2/138/210
 
 # ============活动地块标注
-gmt pstext -R$R -J$J -F+f10p,37 -O -K >> $PS << EOF
+gmt text -F+f10p,37 << EOF
 90 45 西域活动地块
 122 46 东北亚活动地块
 120 35 华北活动地块
@@ -34,7 +30,7 @@ gmt pstext -R$R -J$J -F+f10p,37 -O -K >> $PS << EOF
 EOF
 
 # ============边界图例
-gmt pslegend -R$R -J$J -DjTL+w1.5i+jTL+o0.2c/0.2c -F+gwhite+p0.5p  -O -K --FONT_ANNOT_PRIMARY=8p,37 >> $PS << EOF
+gmt legend -DjTL+w1.5i+jTL+o0.2c/0.2c -F+gwhite+p0.5p --FONT_ANNOT_PRIMARY=8p,37 << EOF
 S 0.3i f0.5+t+l 0.4i/0.3c 2/138/210 2.0p,2/138/210 0.7i 板块边界
 S 0.3i - 0.50i 2/138/210 1.0p,2/138/210 0.7i  一级地块边界
 S 0.3i - 0.44i - 1.0p,2/138/210,- 0.7i  推断地块边界
@@ -42,9 +38,7 @@ S 0.3i - 0.50i orange 1.0p,orange 0.7i 二级地块边界
 EOF
 
 # ============南海诸岛
-gmt psbasemap -R$R -J$J -DjBR+w2c+t -K -O >> $PS
-gmt pscoast -R106/121/3/24 -JM2c -Ggray95 -S83/216/238 -A5000 -Dh -K -O >> $PS
-gmt psxy CN-border-L1.dat -R106/121/3/24 -JM2c -B0 -W0.2p -Gwhite -O -K >> $PS
-
-gmt psxy -R$R -J$J -T -O >> $PS
-rm gmt.*
+gmt basemap -DjBR+w2c+t
+gmt coast -R106/121/3/24 -JM2c -Ggray95 -S83/216/238 -A5000 -Dh 
+gmt plot CN-border-L1.dat -R106/121/3/24 -JM2c -B0 -W0.2p -Gwhite
+gmt end

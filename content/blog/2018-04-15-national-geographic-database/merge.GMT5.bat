81d19a4f08f5fa68392f9d4eb8282d67aed87d5b
Dongdong Tian <seisman.info@gmail.com>
2019-05-20 09:46:22 -0400

    Fix encoding

--- content/blog/2018-04-15-national-geographic-database/merge.GMT5.bat
+++ content/blog/2018-04-15-national-geographic-database/merge.GMT5.bat
@@ -1,8 +1,8 @@
-REM 灏咥49鍒嗗箙鐨刧db鏁版嵁杞负涓�缁剆hp鏍煎紡鐨勬枃浠讹紝鏂囦欢澶瑰懡鍚嶄负Merged
-REM 缂栫爜涓篣TF-8浠ユ纭樉绀哄睘鎬у瓧娈典腑鐨勪腑鏂�
+REM 将A49分幅的gdb数据转为一组shp格式的文件，文件夹命名为Merged
+REM 编码为UTF-8以正确显示属性字段中的中文
 ogr2ogr -f "ESRI Shapefile" -lco encoding=UTF-8 Merged A49.gdb
 
-REM 灏嗗綋鍓嶇洰褰曚笅鎵�鏈夌殑gdb鏁版嵁杩藉姞Merged锛屽悓绫籹hp鏂囦欢鍚堝苟銆�
+REM 将当前目录下所有的gdb数据追加Merged，同类shp文件合并。
 for /f %%i in ('dir /s/b "*.gdb"') do (
-ogr2ogr -f "ESRI Shapefile" -update -append Merged %%i  
+ogr2ogr -f "ESRI Shapefile" -update -append Merged %%i
 )

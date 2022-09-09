d60ca691dd5cbf5f19287da3281df62b6e3da859
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2022-09-09 11:17:36 +0900

    Update merge.GMT6.bat

--- content/blog/2018-04-15-national-geographic-database/merge.GMT6.bat
+++ content/blog/2018-04-15-national-geographic-database/merge.GMT6.bat
@@ -3,6 +3,6 @@ REM 编码为UTF-8以正确显示属性字段中的中文
 ogr2ogr -f "ESRI Shapefile" -lco encoding=UTF-8 Merged A49.gdb
 
 REM 将当前目录下剩余所有的gdb数据追加Merged，同类shp文件合并。
-for /f %%i in ('dir /s/b "*.gdb"') do (
+for /f %%i in ('dir /b "*.gdb"') do (
 ogr2ogr -f "ESRI Shapefile" -update -append Merged %%i
 )
